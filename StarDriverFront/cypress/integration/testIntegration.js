b4fcf7d02c979ca8cb41387206307ef5d5e1a3d9
Gustavo-07 <gustavo-garcia1994@hotmail.com>
2020-12-14 01:54:43 -0500

    add navegation test with cypress

---
+++ StarDriverFront/cypress/integration/testIntegration.js
@@ -0,0 +1,23 @@
+﻿const { createYield } = require("typescript")
+
+describe('TestPaginaView', function(){
+
+  it('navegationTest', function(){
+    cy.visit('http://localhost:4200/Index');
+    cy.get('[ng-reflect-router-link="/index"] > .mat-button-wrapper').click();
+    cy.get('#Btn-CrearPersona').click();
+    cy.get('[mat-dialog-close=""] > .mat-button-wrapper').click();
+    cy.get('#Btn-ActualizarPersona > .mat-button-wrapper > .mat-icon').click();
+    cy.get('[mat-dialog-close=""] > .mat-button-wrapper').click();
+
+    cy.get('[ng-reflect-router-link="/room"] > .mat-button-wrapper').click();
+    cy.get('#btn-crearsalon > .mat-button-wrapper').click();
+    cy.get('[mat-dialog-close=""] > .mat-button-wrapper').click();
+
+  });
+
+  it('testSavePerson', function(){
+
+  });
+
+});
